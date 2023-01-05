3c0a9686c128b61d207dd14da66ade8574197062
GUPTARAHULONE <guptarahul26318@gmail.com>
2023-01-05 18:01:55 +0530

    Expect and Actual Database Class for both platforms

---
+++ shared/src/iosMain/kotlin/com/example/translator_kmm_app/translate/data/local/DatabaseDriverFactory.kt
@@ -0,0 +1,11 @@
+package com.example.translator_kmm_app.translate.data.local
+
+import com.example.translator_kmm_app.database.TranslateDatabase
+import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
+
+actual class DatabaseDriverFactory {
+    actual fun create(): SqlDriver {
+        return NativeSqliteDriver(TranslateDatabase.Schema, "translate.db")
+    }
+}
